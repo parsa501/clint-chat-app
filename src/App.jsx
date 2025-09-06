4ba9a2bee668a24dedd0fa014f441e12712eae81
parsa501 <safazadehparsa@gmail.com>
2025-09-06 17:44:02 -0500

    add route

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './Pages/Home'
+import Login from './Pages/Login'
+import Profile from './Pages/Profile'
 
 export default function App() {
   return (
-    <div>
-      
+    <div className="bg-[url('./src/assets/bgImage.svg')]">
+     <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/profile' element={<Profile/>}/>
+      </Routes> 
     </div>
   )
 }
